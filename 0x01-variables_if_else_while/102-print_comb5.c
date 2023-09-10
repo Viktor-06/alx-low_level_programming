ffcded9289d652e544bf19b28f03129e7dd4211e
Victor Nwodo <viknwodo@gmail.com>
2023-09-10 08:11:15 -0700

    C variables if, else, while. Advance task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,25 @@ int main(void)
 
 {
 
-	int d, p;
+	int p, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (p = 0; p <= 98; p++)
 	{
 
-	for (p = d + 1; p <= '9'; p++)
+	for (q = p + 1; q <= 99; q++)
 	{
-	if (p != d)
-	{
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p =='9')
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	}
 	putchar('\n');
 	return (0);
 }
+
