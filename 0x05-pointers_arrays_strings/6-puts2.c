29f34341513f7d9a94a1dca88bd2cae1c6113ded
Victor Nwodo <viknwodo@gmail.com>
2023-09-18 18:34:57 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		
+	}
+}	
